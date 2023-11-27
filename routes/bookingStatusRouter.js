56f00c5eee24d84129be739867b9ad5d57fc5921
Anh Thu <thuthuanh19@gmail.com>
2023-11-27 16:46:48 +0700

    update from origin

---
+++ routes/bookingStatusRouter.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = express.Router();
+const bookingStatusController = require("../controllers/bookingStatusController");
+
+// Create a new BookingStatusId
+router.post("/create", async (req, res) => {
+  const { status_description } = req.body;
+
+  try {
+    const newBookingStatus = await bookingStatusController.createBookingStatus(
+      status_description
+    );
+    res
+      .status(201)
+      .json({
+        message: "Booking status created successfully",
+        bookingStatus: newBookingStatus,
+      });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Get all BookingStatusIds
+router.get("/all", async (req, res) => {
+  try {
+    const allBookingStatuses =
+      await bookingStatusController.findAllBookingStatuses();
+    res.status(200).json(allBookingStatuses);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Get BookingStatusId by ID
+router.get("/:id", async (req, res) => {
+  const statusId = req.params.id;
+
+  try {
+    const bookingStatus = await bookingStatusController.findBookingStatusById(
+      statusId
+    );
+
+    if (!bookingStatus) {
+      res.status(404).json({ message: "Booking status not found" });
+    } else {
+      res.status(200).json(bookingStatus);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Update a booking form
+router.put("/update/:id", async (req, res, next) => {
+  const bookingFormId = req.params.id;
+  const updatedData = req.body; // The updated data provided in the request body
+
+  try {
+    const updatedBookingForm =
+      await bookingFormController.updateBookingFormById(
+        bookingFormId,
+        updatedData
+      );
+    res.status(200).json(updatedBookingForm);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
